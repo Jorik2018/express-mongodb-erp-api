9f100cdde9290bacb3b00e55e8a4ec2213b81efe
Jorik2018 <antoniopinedo2018@gmail.com>
2025-03-29 23:56:04 -0500

    x

--- src/routes/facebook.ts
+++ src/routes/facebook.ts
@@ -6,12 +6,27 @@ const router = Router();
 const { FB_APP_ID, FB_APP_SECRET, FB_REDIRECT_URI } = process.env;
 
 // Initiates the Facebook Login flow
-router.get('', (req, res) => {
+router.get('/facebook', (req, res) => {
     const url = `https://www.facebook.com/v13.0/dialog/oauth?client_id=${FB_APP_ID}&redirect_uri=${FB_REDIRECT_URI}&scope=email`;
     res.redirect(url);
 });
 
-// Callback URL for handling the Facebook Login response
+router.post('/token', async ({ body: { code, provider } }, res) => {
+    try {
+        console.log(code, provider)
+        const { data } = await axios.get(`https://graph.facebook.com/v13.0/oauth/access_token?client_id=${FB_APP_ID}&client_secret=${FB_APP_SECRET}&code=${code}&redirect_uri=${FB_REDIRECT_URI}`);
+        const { access_token } = data;
+        // Use access_token to fetch user profile
+        const { data: profile } = await axios.get(`https://graph.facebook.com/v13.0/me?fields=name,email&access_token=${access_token}`);
+        // Code to handle user authentication and retrieval using the profile data
+        //res.redirect('/');
+        res.send(profile);
+    } catch (error: any) {
+        console.error('Error:', error.response.data.error);
+        //res.redirect('/login');
+    }
+});
+
 router.get('/callback', async (req, res) => {
     const { code } = req.query;
 
@@ -26,10 +41,10 @@ router.get('/callback', async (req, res) => {
 
         // Code to handle user authentication and retrieval using the profile data
 
-        res.redirect('/');
+        //res.redirect('/');
     } catch (error: any) {
         console.error('Error:', error.response.data.error);
-        res.redirect('/login');
+        //res.redirect('/login');
     }
 });
 
