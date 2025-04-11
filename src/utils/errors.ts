344bfacd359f5bb12393765df4e9e3f4454a4c15
Jorik2018 <antoniopinedo2018@gmail.com>
2025-04-10 23:31:10 -0500

    x

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -3,10 +3,12 @@ import { Response, NextFunction } from "express";
 export const sendError = (target: NextFunction | Response, code?: number) => (err: any) => {
   if ((target as Response).send) {
     const status = code || err.statusCode || 500;
-    const message = err.message;
-    console.error(err,err.stack);
+    const message = typeof err === 'string' ? err : err.message;
+    const stack = typeof err === 'string' ? null : err.stack;
+    const data = typeof err === 'string' ? null : err.data;
+    console.error(err);
     (target as Response).status(status).json(
-      { message, data: err.data }
+      { message, data }
     );
   } else {
     if (!err.statusCode) {
