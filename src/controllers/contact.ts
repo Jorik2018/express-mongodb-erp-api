44538e36a17d2912119276dd6b0ffae2b777812e
Jorik2018 <antoniopinedo2018@gmail.com>
2025-04-29 23:08:14 -0500

    x

--- src/controllers/contact.ts
+++ src/controllers/contact.ts
@@ -1,17 +1,15 @@
 import { Request, Response } from 'express'
 import Contact from '../database/models/contact';
 import { sendError } from '../utils/errors';
+import contactService from '../services/contactService';
+import { RequestWithUserId } from '../auth/is-auth';
 
-const find = (req: Request, res: Response) => {
-    Contact.findOne({
-        _id: req.params.id
-    })
-        .then(({ _doc: contact }: any) => res.send({ rating: 0, ...contact }))
+const find = ({ params: { id }, userId }: RequestWithUserId, res: Response) => {
+    contactService.find(id, userId)
         .catch(sendError(res));
 };
 
-
-const list = (req: Request, res: Response) => {
+const list = (req: RequestWithUserId, res: Response) => {
     Contact.find({})
         .then((data: any[]) => {
             res.status(200).send({
---
+++ src/services/contactService.ts
@@ -0,0 +1,67 @@
+import { Types } from "mongoose";
+import Campaign from "../database/models/campaign";
+import User, { IUser } from "../database/models/user";
+import { moveTmp } from "../controllers/upload";
+import Contact from "../database/models/contact";
+
+const list = ({ userId }: any) => {
+
+}
+
+const find = (_id: string, userId: string) => {
+    const _user = Types.ObjectId.createFromHexString(userId);
+    return Contact.findOne({
+        _id
+    }).populate('user').lean()
+        .then(({ user, _id, ...contact }: any) => {
+            if (!user._id.equals(_user)) {
+                throw { code: 401, message: "Unauthorized" };
+            }
+            return {
+                rating: 0, ...contact
+            }
+        })
+}
+
+const create = ({ brandId, userId, coverImage, gallery, ...body }: any) => {
+    const user = Types.ObjectId.createFromHexString(userId);
+    const brand = Types.ObjectId.createFromHexString(brandId);
+    return new Campaign({ ...body, user, brand, gallery: [], coverImage: '?' })
+        .save()
+        .then(({ _doc: { _id, ...campaign } }: any) => {
+            const id = _id.toHexString();
+            return moveTmp([coverImage], 'campaign', id).then(([coverImage]: any) =>
+                moveTmp(gallery, 'campaign', id).then((gallery) =>
+                    Campaign.findOneAndUpdate({ _id },
+                        { $set: { coverImage, gallery } }).then(() => ({ id, ...campaign }))
+                )
+            )
+        });
+};
+
+const update = (_id: string, { brandId, userId, coverImage, gallery, ...body }: any) => {
+    return Campaign.find({ _id }).lean().then(({ _id, ...campaign }: any) => {
+        const id = _id.toHexString();
+        return moveTmp([coverImage], 'campaign', id).then(([coverImage]: any) =>
+            moveTmp(gallery, 'campaign', id).then((gallery) =>
+                Campaign.updateOne({ _id },
+                    { $set: { coverImage, gallery, ...campaign } }).then(() => ({ id, coverImage, gallery, ...campaign }))
+            )
+        )
+    });
+};
+
+const activate = (_id: string) => {
+    return Campaign.findByIdAndUpdate({ _id }, { $set: { status: 'active' } }, { new: true },).lean().then(({ status }: any
+
+    ) => ({ status }))
+
+};
+
+const remove = (_id: string) => {
+    return Campaign.findOneAndDelete({
+        _id
+    })
+};
+
+export default { list, find, create, update, remove, activate }
