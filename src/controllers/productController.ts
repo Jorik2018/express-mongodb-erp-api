80a32c0f6f1b35b35648777d6a4fb88c30fab5e4
Jorik2018 <antoniopinedo2018@gmail.com>
2025-05-04 13:58:54 -0500

    x

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -10,48 +10,21 @@ router.get("/product/:id", ({ params: { id } }: Request, res: Response) =>
 );
 
 router.get("/products", ({ query }: Request, res: Response) => {
-    productService.list(query).then(sendJson(res)).catch(sendError(res));
+    productService.list(query).then((data) => sendJson(res)({ data })).catch(sendError(res));
 });
 
-router.post("/product", (req: Request, res: Response) => {
-    let newProduct = req.body;
-    inventoryDB.insertAsync(newProduct).then((product: any) => {
-        res.send(product);
-    }).catch(sendError(res));
+router.post("/product", ({ body }: Request, res: Response) => {
+    productService.create(body).then(sendJson(res)).catch(sendError(res));
 });
 
 router.delete("/product/:id", ({ params: { id } }: Request, res: Response) => {
-    inventoryDB.removeAsync({
-        _id: id
-    }, {}).then(sendJson(res)).catch(sendError(res));
+    productService.remove(id).then(sendJson(res)).catch(sendError(res));
 });
 
 router.put("/product", ({ body }: Request, res: Response) => {
-    let { id, ...product } = body;
-    inventoryDB.updateAsync({ _id: id }, product, {}).then(sendJson(res)).catch(sendError(res));;
+    productService.update(body).then(sendJson(res)).catch(sendError(res));
 });
 
-(router as any).decrementInventory = (products: any) => {
-    async.eachSeries(products, (transactionProduct: any, callback: any) => {
-        inventoryDB.findOneAsync({
-            _id: transactionProduct._id
-        }).then((product: any) => {
-            if (!product || !product.quantity_on_hand) {
-                callback();
-            } else {
-                let updatedQuantity =
-                    parseInt(product.quantity_on_hand) -
-                    parseInt(transactionProduct.quantity);
-                inventoryDB.updateAsync({
-                    _id: product._id
-                }, {
-                    $set: {
-                        quantity_on_hand: updatedQuantity
-                    }
-                }).then(callback);
-            }
-        });
-    });
-};
+(router as any).decrementInventory = productService.decrementInventory;
 
 export default router;
